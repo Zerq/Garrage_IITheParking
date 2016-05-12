5666df89c7bca4cdd666788cf8b76bc1ac5ffee5
Zerq <Zerq_tm@hotmail.com>
2016-05-12 12:05:26 +0200

    basic objects done

--- GarageII_TheParking/GarageII_TheParking/Models/Vehicle.cs
+++ GarageII_TheParking/GarageII_TheParking/Models/Vehicle.cs
@@ -4,9 +4,56 @@ using System.Linq;
 using System.Web;
 
 namespace GarageII_TheParking.Models {
-    public class Vehicle {
-        
-        public string Color { get; set; }
 
+    [Flags]
+    public enum Colors {
+        Brown=0,
+        DarkBrown=1,
+        LightBrown=2<<0,
+        Yellow= 2 << 1,
+        DarkYellow= 2 << 2,
+        LightYellow= 2 << 3,
+        Red= 2 << 4,
+        DarkRed= 2 << 5,
+        LightishRed = 2 <<6,
+        Purple = 2 <<7,
+        DarkPurple = 2 <<8,
+        LightPurple = 2 <<9,
+        Blue = 2 <<10,
+        DarkBlue = 2 <<11,
+        LightBlue = 2 <<12,
+        Teal = 2 <<13,
+        DarkTeal = 2 <<14,
+        LightTeal = 2 <<15,
+        Green =2 <<16,
+        DarkGreen = 2 <<17,
+        LightGreen = 2 <<18,
+        Silver = 2 <<19,
+        Gold = 2 <<20,
+        Black = 2 <<21,
+        DarkGray = 2 <<22,
+        Gray = 2 <<23,
+        LightGray = 2 <<24,
+        White = 2 <<25,
+    }
+
+
+    public enum VehicleType {
+        Car,MotorCycle,Buss,Truck,Tractor
+    }
+    public class Vehicle {
+        public Guid Id { get; set; }
+        public Colors Color { get; set; }
+        public string RegistrationNumber { get; set; }
+        public VehicleType Type { get; set; }
+        public int WheelCount { get; set; }
+        public string Brand { get; set; }
+        public DateTime ParkedDate { get; set; }
+    }
+
+
+    public class Garage {
+        public Guid Id { get; set; }
+        public List<Vehicle> Vehicles { get; set; }
     }
 }

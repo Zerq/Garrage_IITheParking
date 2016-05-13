5ac1ad2f68675008a95117991d5a38d291cecc79
Zerq <Zerq_tm@hotmail.com>
2016-05-13 10:05:29 +0200

    Ok now i am happy

--- GarageII_TheParking/GarageII_TheParking/Handler/GarageHandler.cs
+++ GarageII_TheParking/GarageII_TheParking/Handler/GarageHandler.cs
@@ -1,24 +1,22 @@
 ﻿using GarageII_TheParking.DataAccessLayer;
+using GarageII_TheParking.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using GarageII_TheParking.Models;
 
 namespace GarageII_TheParking.Handler {
 
     public class GarageHandler : GarageHandlerbase {
         public override Receipt Collect(Vehicle vehicle) {
             throw new NotImplementedException();
-
-           // this.GetGarage()
         }
 
         public override List<Vehicle> ListParkedVehicles() {
             throw new NotImplementedException();
         }
 
-        public override void Park(Vehicle vehicle) {
+        public override Receipt Park(Vehicle vehicle) {
             throw new NotImplementedException();
         }
     }
--- GarageII_TheParking/GarageII_TheParking/Handler/GarageHandlerbase.cs
+++ GarageII_TheParking/GarageII_TheParking/Handler/GarageHandlerbase.cs
@@ -9,23 +9,35 @@ namespace GarageII_TheParking.Handler {
 
     public abstract class GarageHandlerbase {
 
-        Context db = new Context();
-        protected IEnumerable<Models.Garage> GetGarage() {
-            // get our currently singular garage out of the database...
-            return db.Garage.Where(n => n.Id == Guid.Empty);
+       protected Context db = new Context();
+
+        private Models.Garage garage;
+        protected Models.Garage Garage {
+           get {
+                if (garage == null) {
+                   garage = db.Garage.Where(n => n.Id == Guid.Empty).First();
+                }
+                return garage;
+            }            
         }
 
-        public abstract void Park(Models.Vehicle vehicle);
+        public abstract Receipt Park(Models.Vehicle vehicle);
+
+       /* vehicle.ParkedDate = DateTime.Now;
+            
+            garage.Vehicle.Add(vehicle);
+            db.Entry(this.Garage).State == System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();_*/
 
 
-        public abstract Receipt Collect(Models.Vehicle vehicle);
+        public  abstract Receipt Collect(Models.Vehicle vehicle);
         // remove car from garage
         // and figure out some result to return if the car was parked to long!
         // a recite or something... that or chuck some annoyed badgers at the user or something....
 
 
 
-        public abstract List<Models.Vehicle> ListParkedVehicles();
+        public abstract  List<Models.Vehicle> ListParkedVehicles();
 
 
     }

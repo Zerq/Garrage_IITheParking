430a6da7ec7f69032c96ad77503edff59b0f64d4
Zerq <Zerq_tm@hotmail.com>
2016-05-13 10:13:06 +0200

    changed receipt to be in viewmodels since i think it wont get stored in the database.. at least not yet

--- GarageII_TheParking/GarageII_TheParking/Models/ViewModels/Receipt.cs
+++ GarageII_TheParking/GarageII_TheParking/Models/ViewModels/Receipt.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace GarageII_TheParking.Models {
+namespace GarageII_TheParking.Models.ViewModels {
     public class Receipt {
     }
 }
